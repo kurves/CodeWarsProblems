ecc510cba9bc61fa0b902f55e1f251a8a021fbdb
kurves <kavssimply@gmail.com>
2024-01-29 17:23:25 +0300

    function to get past time

--- pastClock.py
+++ pastClock.py
@@ -2,6 +2,7 @@ def pastClock(h, m, s):
     hour = 60 * 60 * 1000
     min = 60 * 1000
     sec = 1000
-    print(h* hour +m* min + s*sec)
+    timepast = (h* hour +m* min + s*sec)
+    return timepast
 
 pastClock(0,1,1)
