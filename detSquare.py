d18f680d1826b127e413aaf9622b2eb8f1842094
kurves <kavssimply@gmail.com>
2024-02-02 17:50:54 +0300

    function to get square

--- detSquare.py
+++ detSquare.py
@@ -1,6 +1,8 @@
+import math
 def detSquare(num):
-    n = Mrth.sqrt(num)
+    n = math.sqrt(num)
     if num % n == 0:
         print("true") 
-    if num * n == num
-    return True
+    
+
+detSquare(-1)
